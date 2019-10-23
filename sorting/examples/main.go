7c8e13724b9125b831e2100c62e3ab745628f970
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2019-10-23 08:38:45 +0100

    Added examples

--- sorting/examples/main.go
+++ sorting/examples/main.go
@@ -15,4 +15,10 @@ func main() {
 	fmt.Println("============ Insertion Sort ========= ")
 	c := sorting.InsertionSort([]int{7,2,3,4,1})
 	fmt.Println(c)
+	fmt.Println(" =============== Merge Sort ========== ")
+	d := sorting.MergeSort([]int{1, 2, 4768, 56, 23, 56,12, 45, 6, 8, 9 ,0, 7,2,3,4,1})
+	fmt.Println(d)
+	fmt.Println(" =============== Quick Sort ========== ")
+	e := sorting.QuickSort([]int{1, 2, 4768, 56, 23, 56,12, 45, 6, 8, 9 ,0, 7,2,3,4,1})
+	fmt.Println(e)
 }
